01744553e5c09314aeaf275f52daee284cbffa48
d-arsya <kamaluddin.arsyad17@gmail.com>
2024-06-14 20:30:42 +0700

    filter feature added

--- src/Home.java
+++ src/Home.java
@@ -1,5 +1,6 @@
 import org.jdesktop.swingx.JXDatePicker;
 import config.DbConnect;
+import javax.sound.sampled.BooleanControl;
 import javax.swing.*;
 import java.awt.*;
 import java.sql.*;
@@ -7,76 +8,154 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimerTask;
 
 public class Home extends JFrame{
-    ResultSet dataFromDB = null;
+    Color warna = Color.RED;
+    DbConnect con = new DbConnect();
     Statement statement = null;
-    DbConnect con = null;
+    ResultSet dataFromDB = null;
+    private JXDatePicker inputTanggal = new JXDatePicker(new Locale("id","ID"));
+    private JCheckBox cekAc = new JCheckBox("AC");
+    private JCheckBox cekDouble = new JCheckBox("Double Bed");
     private ArrayList<RoomClass> daftarKamar = new ArrayList<RoomClass>();
     private JLabel timeLabel = new JLabel();
     private JLabel dateLabel = new JLabel();
-    private  JScrollPane contKamar = new JScrollPane();
+    private  JScrollPane contMain = new JScrollPane();
     private SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
     private SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, dd MMMM yyyy", new Locale("id", "ID"));
 
     public Home(){
-        try{
-            con = new DbConnect();
+        try {
             statement = con.getConnection().createStatement();
-            dataFromDB = statement.executeQuery("SELECT * FROM dataKamar");
-            while(dataFromDB.next()){
-                RoomClass baru = new RoomClass(dataFromDB.getString(2),dataFromDB.getInt(3),dataFromDB.getString(4),dataFromDB.getString(5),dataFromDB.getInt(6));
-                daftarKamar.add(baru);
-            }
-            System.out.println(daftarKamar.size());
-        }catch (SQLException e){
-            System.out.println(e.toString());
+        } catch (SQLException e) {
+            System.out.println(e);
         }
+        updateKamar();
         init();
 
     }
+    private void updateKamar(){
+        String query = "CALL get_available_rooms('"+new SimpleDateFormat("yyyy-MM-dd").format(new Date())+" 00:00:00','1','0')";
+        try{
+            dataFromDB = statement.executeQuery(query);
+            while(dataFromDB.next()){
+                RoomClass baru = new RoomClass(dataFromDB.getString(1),dataFromDB.getInt(2),dataFromDB.getInt(3),dataFromDB.getInt(4));
+                daftarKamar.add(baru);
+            }
+        }catch (SQLException e){
+            System.out.println(e.toString());
+        }
+    }
+    private void updateKamar(JPanel cont){
+        cont.removeAll();
+        daftarKamar.clear();
+        String tanggal = new SimpleDateFormat("yyyy-MM-dd").format(inputTanggal.getDate());
+        String acC = cekAc.isSelected()?"1":"0";
+        String doubleBe = cekDouble.isSelected()?"2":"1";
+        String query = "CALL get_available_rooms('"+tanggal+" 00:00:00','"+doubleBe+"','"+acC+"')";
+        try{
+            dataFromDB = statement.executeQuery(query);
+            while(dataFromDB.next()){
+                RoomClass baru = new RoomClass(dataFromDB.getString(1),dataFromDB.getInt(2),dataFromDB.getInt(3),dataFromDB.getInt(4));
+                daftarKamar.add(baru);
+            }
+        }catch (SQLException e){
+            System.out.println(e.toString());
+        }
+        Timer t = new Timer(100, new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cont.removeAll();
+                for(RoomClass kamar:daftarKamar){
+                    cont.add(kamar.createCard());
+                }          
+            }
+        });
+        t.setRepeats(false);
+        t.start();
+    }
+    public class CustomCheckBoxIcon implements Icon {
+        private int size;
+        
+
+        public CustomCheckBoxIcon(int size) {
+            this.size = size;
+        }
+
+        @Override
+        public void paintIcon(Component c, Graphics g, int x, int y) {
+            JCheckBox cb = (JCheckBox) c;
+            if (cb.isSelected()) {
+                g.setColor(Color.BLACK);
+                g.fillArc(x,y,size-1,size-1,0,360);
+            } else {
+                g.setColor(Color.WHITE);
+                g.fillArc(x,y,size-1,size-1,0,360);
+//                g.drawRect(x, y, size - 1, size - 1);
+            }
+        }
+
+        @Override
+        public int getIconWidth() {
+            return size;
+        }
+
+        @Override
+        public int getIconHeight() {
+            return size;
+        }
+    }
+
     private void init(){
         JPanel contJam = new JPanel(null);
         JPanel contDetails = new JPanel(null);
+        JPanel contMain = new JPanel(null);
+        JPanel contInput = new JPanel();
+        JPanel contButton = new JPanel(new GridLayout(9,1,0,20));
+        JPanel contKamar = new JPanel(null);
+        // JPanel contKamar = new JPanel(new FlowLayout(FlowLayout.LEFT,10,10));
+
         JLabel labelNama = new JLabel("Hotel Disyfa");
         JLabel labelKamar = new JLabel("Sisa Kamar");
         JLabel labelPengunjung = new JLabel("Pengunjung");
         JLabel dataKamar = new JLabel("10");
         JLabel dataPengunjung = new JLabel("20");
-        JPanel contButton = new JPanel(new GridLayout(9,1,0,20));
-        JButton btnAdmin = new JButton("Admin");
+
+        JButton btnAdmin = new JButton("GATAU");
         JButton btnCustomers = new JButton("Customers");
         JButton btnRooms = new JButton("Rooms");
-        JTextField inputIn = new JTextField("Check IN : ");
-        JTextField inputOut = new JTextField("Check OUT : ");
         JButton btnLogout = new JButton("Logout");
         JButton btnExit = new JButton("Keluar");
-        JPanel contKamar = new JPanel(null);
-        JPanel contInput = new JPanel();
-        JXDatePicker inputTanggal = new JXDatePicker(new Locale("id","ID"));
+        JTextField inputIn = new JTextField("Check IN : ");
+        JTextField inputOut = new JTextField("Check OUT : ");
+
+
         contJam.setBounds(0,0,350,160);
         contJam.setBackground(Color.ORANGE);
+        
 
         contDetails.setBounds(WindowSize.width-350,0,350,160);
         contDetails.setBackground(Color.ORANGE);
 
         contButton.setBounds(WindowSize.width-350,200,350,650);
-        contButton.setBackground(new Color(0,0,0,0));
-        contButton.setOpaque(true);
+        contButton.setBackground(warna);
         contButton.setBorder(BorderFactory.createEmptyBorder(0,30,0,67));
+        
+        contMain.setBounds(67,200,1120,650);
+        contKamar.setBounds(0,100,1120, 1000);
+        contKamar.setLayout(new FlowLayout(FlowLayout.LEADING,20,20));
+        contKamar.setBorder(BorderFactory.createEmptyBorder(30, 80, 0, 80));
 
-        contKamar.setBounds(67,200,1120,650);
-
-        contInput.setLayout(new BoxLayout(contInput,BoxLayout.X_AXIS));
+        contInput.setLayout(new GridLayout(1,3,0,0));
+        contInput.setBorder(BorderFactory.createEmptyBorder(10,50,10,350));
         contInput.setBounds(0,0,1120,100);
-        contInput.setBackground(Color.BLUE);
-        labelNama.setBounds(350,0,WindowSize.width-700,160);
+        labelNama.setBounds(350,0, WindowSize.width-700,160);
         labelNama.setFont(new Font("Inter", Font.BOLD,48));
         labelNama.setHorizontalAlignment(JLabel.CENTER);
 
@@ -100,15 +179,26 @@ public class Home extends JFrame{
         inputIn.setFont(new Font("Inter", Font.ITALIC,20));
         inputOut.setFont(new Font("Inter", Font.ITALIC,20));
 
+        cekAc.setFont(new Font("Inter", Font.ITALIC,30));
+        cekAc.setIcon(new CustomCheckBoxIcon(30));
+
+        cekDouble.setFont(new Font("Inter", Font.ITALIC,30));
+        cekDouble.setIcon(new CustomCheckBoxIcon(30));
+
+
         inputTanggal.setBounds(0,0,300,100);
         inputTanggal.getEditor().setEditable(false);
-        inputTanggal.getEditor().setText(null);
-        inputTanggal.setDate(new Date());
+        // inputTanggal.getEditor().setText(null);
         inputTanggal.setBorder(BorderFactory.createEmptyBorder(10,10,20,10));
         inputTanggal.setBackground(Color.RED);
+        inputTanggal.setFont(new Font("Inter", Font.ITALIC,20));
+        inputTanggal.setDate(new Date());
 
+        for(RoomClass kamar:daftarKamar){
+            contKamar.add(kamar.createCard());
+        }
 
-
+        contKamar.setBackground(Color.GREEN);
         btnLogout.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -186,6 +276,24 @@ public class Home extends JFrame{
 
             }
         });
+        cekAc.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateKamar(contKamar);
+            }
+        });
+        cekDouble.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateKamar(contKamar);
+            }
+        });
+        inputTanggal.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateKamar(contKamar);
+            }
+        });
 
 
         timeLabel.setFont(new Font("Inter", Font.BOLD,20));
@@ -203,13 +311,14 @@ public class Home extends JFrame{
         timer.start();
 
         updateTime();
-
         contJam.add(dateLabel);
         contJam.add(timeLabel);
+
         contDetails.add(labelKamar);
         contDetails.add(labelPengunjung);
         contDetails.add(dataKamar);
         contDetails.add(dataPengunjung);
+
         contButton.add(btnAdmin);
         contButton.add(btnCustomers);
         contButton.add(btnRooms);
@@ -219,26 +328,28 @@ public class Home extends JFrame{
         contButton.add(new JLabel());
         contButton.add(btnLogout);
         contButton.add(btnExit);
-        contKamar.add(contInput);
+
+        contMain.add(contInput);
+        contMain.add(contKamar);
+
         contInput.add(inputTanggal);
+        contInput.add(cekAc);
+        contInput.add(cekDouble);
+
         add(contJam);
         add(contDetails);
         add(labelNama);
         add(contButton);
-        add(contKamar);
+        add(contMain);
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(WindowSize.width,WindowSize.heigth);
+        setSize(WindowSize.width, WindowSize.heigth);
         setUndecorated(true);
         setLayout(null);
-        getContentPane().setBackground(Color.RED);
+        getContentPane().setBackground(warna);
         setVisible(true);
     }
-    private void updateKamar(){
-        for(RoomClass kamar:daftarKamar){
-            JLabel nama = new JLabel(kamar.getRoomNumber());
 
-        }
-    }
     private void updateTime() {
         String currentTime = timeFormat.format(new Date());
         String currentDate = dateFormat.format(new Date());
@@ -252,7 +363,6 @@ public class Home extends JFrame{
                     UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
-                System.out.println(info.getName());
             }
         }catch (Exception e){
             System.out.println(e.toString());
@@ -264,3 +374,4 @@ public class Home extends JFrame{
         }
     }
 }
+
--- src/RoomClass.java
+++ src/RoomClass.java
@@ -1,3 +1,8 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.*;
 public class RoomClass {
     private String roomNumber;
@@ -6,11 +11,10 @@ public class RoomClass {
     private boolean available;
     private int price;
 
-    public RoomClass(String roomNumber,int bed,String ac,String available,int price){
+    public RoomClass(String roomNumber,int bed,int ac,int price){
         this.roomNumber = roomNumber;
         this.doubleBed = bed==2;
-        this.ac = ac.equals("true");
-        this.available = available.equals("available");
+        this.ac = ac==1;
         this.price = price;
     }
 
@@ -29,9 +33,49 @@ public class RoomClass {
     public int getPrice() {
         return price;
     }
-//    public JPanel createCard(){
-//        JPanel container = new JPanel;
-//
-//        return container;
-//    }
+   public JPanel createCard(){
+       JPanel container = new JPanel();
+       JPanel contLeft = new JPanel();
+       JLabel nomorKamar = new JLabel(this.roomNumber);
+       JLabel acLabel = new JLabel(this.ac?"AC":"NON-AC");
+       JLabel doubleLabel = new JLabel(this.doubleBed?"Double Bed":"Single Bed");
+       JLabel priceLabel = new JLabel(Integer.toString(this.price));
+
+       container.setLayout(new BoxLayout(container,BoxLayout.X_AXIS));
+       contLeft.setLayout(new BoxLayout(contLeft,BoxLayout.Y_AXIS));
+       contLeft.setBorder(BorderFactory.createEmptyBorder(0,00,0,60));
+       contLeft.setBackground(new Color(0,0,0,0));
+       contLeft.setOpaque(true);
+       container.setBorder(BorderFactory.createEmptyBorder(0,30,0,30));
+       
+       nomorKamar.setSize(150,40);
+       nomorKamar.setFont(new Font("Inter", Font.BOLD,24));
+       
+       priceLabel.setSize(120,40);
+       priceLabel.setFont(new Font("Inter", Font.BOLD,24));
+       
+       acLabel.setSize(120,40);
+       acLabel.setFont(new Font("Inter", Font.BOLD,18));
+       
+       doubleLabel.setSize(120,40);
+       doubleLabel.setFont(new Font("Inter", Font.BOLD,18));
+       
+       container.setPreferredSize(new Dimension(300,90));;
+       container.setBackground(Color.BLUE);
+
+       contLeft.add(nomorKamar);
+       contLeft.add(acLabel);
+       contLeft.add(doubleLabel);
+       container.add(contLeft);
+       container.add(priceLabel);
+
+       container.addMouseListener(new MouseAdapter() {
+        public void mouseClicked(MouseEvent me){
+            System.out.println(roomNumber);
+        }
+       });
+
+
+       return container;
+   }
 }
